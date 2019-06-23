3e3581383a41f2255f03a8f3997dc36e06f0f453
Jefferson Rylee <rylee.jeff385@gmail.com>
2019-06-23 18:30:00 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,76 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/strongly-recommended',
     'eslint:recommended'
   ],
   rules: {
+    'array-bracket-newline': ['error', { multiline: true }],
+    'array-element-newline': ['error', 'consistent'],
+    'comma-dangle': ['error', 'never'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'key-spacing': ['error', { afterColon: true }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-nested-ternary': 'off',
+    'object-curly-newline': ['error', { consistent: true }],
+    'object-curly-spacing': [
+      'error',
+      'always',
+      {
+        arraysInObjects: true,
+        objectsInObjects: true
+      }
+    ],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: false
+      }
+    ],
+    'quote-props': ['error', 'as-needed'],
+    semi: ['error', 'never'],
+    // vue
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'never'
+      }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/multiline-html-element-content-newline': ['error', 'always'], // off
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    'vue/singleline-html-element-content-newline': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
